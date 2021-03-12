261ca5a60b48ac812bba7c97a02860524c41ead6
mariliatsgmacedo <maritamisguemacedo@gmail.com>
2021-03-12 15:16:29 +0000

    refactor:databinding configured

--- app/src/main/java/com/macedos/mytasksfinalproject/ui/MainActivity.kt
+++ app/src/main/java/com/macedos/mytasksfinalproject/ui/MainActivity.kt
@@ -2,20 +2,29 @@ package com.macedos.mytasksfinalproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
 import com.macedos.mytasksfinalproject.R
+import com.macedos.mytasksfinalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     //Variables
     private lateinit var firebaseAnalytics: FirebaseAnalytics
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        // Obtain the FirebaseAnalytics instance.
+        // Obtain the FirebaseAnalytics instance
         firebaseAnalytics = Firebase.analytics
+
+        //TabLayout Configuration
+        val tabLayout = binding.tabLayout
+
+        //ViewPager Configuration
+        val viewPager = binding.myViewPager
     }
 }
